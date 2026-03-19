a01e73e7c872fa8ffe86f3a5860dbdb2951ad4ca
hamzaeddr <hamzadour06@gmail.com>
2026-03-19 09:19:18 +0100

    Add FAQ accordion functionality to basic pages

--- web/sites/default/modules/basic_page/src/Utility/Page.php
+++ web/sites/default/modules/basic_page/src/Utility/Page.php
@@ -98,6 +98,65 @@ class Page {
       $data['notizie_sidebar_refs'] = $node->get('field_notizie_sidebar')->getValue();
     }
 
+    // FAQ field (Domande Frequenti)
+    if ($node->hasField('field_domande_frequenti') && !$node->get('field_domande_frequenti')->isEmpty()) {
+      $faq_items = [];
+      $raw_values = $node->get('field_domande_frequenti')->getValue();
+      
+      foreach ($raw_values as $delta => $value) {
+        $faq_item = [];
+        
+        // Handle different field structures
+        if (isset($value['target_id'])) {
+          // Entity reference - load the referenced entity
+          $entity_type = $node->get('field_domande_frequenti')->getFieldDefinition()->getSetting('target_type');
+          $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($value['target_id']);
+          
+          if ($entity) {
+            // Try to get question and answer from the entity
+            $question_fields = ['field_question', 'field_domanda', 'field_titolo', 'field_title', 'name', 'title'];
+            $answer_fields = ['field_answer', 'field_risposta', 'field_body', 'body', 'field_description'];
+            
+            foreach ($question_fields as $field_name) {
+              if ($entity->hasField($field_name) && !$entity->get($field_name)->isEmpty()) {
+                $faq_item['question'] = $entity->get($field_name)->value;
+                break;
+              }
+            }
+            
+            foreach ($answer_fields as $field_name) {
+              if ($entity->hasField($field_name) && !$entity->get($field_name)->isEmpty()) {
+                $faq_item['answer'] = $entity->get($field_name)->value;
+                break;
+              }
+            }
+          }
+        }
+        elseif (isset($value['value'])) {
+          // Simple text field - use value as question
+          $faq_item['question'] = $value['value'];
+          $faq_item['answer'] = isset($value['format']) ? '' : $value['value'];
+        }
+        elseif (isset($value['question']) && isset($value['answer'])) {
+          // Field with question/answer structure
+          $faq_item['question'] = $value['question'];
+          $faq_item['answer'] = $value['answer'];
+        }
+        
+        // Add item if we have at least a question
+        if (!empty($faq_item['question'])) {
+          if (empty($faq_item['answer'])) {
+            $faq_item['answer'] = '';
+          }
+          $faq_items[] = $faq_item;
+        }
+      }
+      
+      if (!empty($faq_items)) {
+        $data['faq'] = $faq_items;
+      }
+    }
+
     return $data;
   }
 
